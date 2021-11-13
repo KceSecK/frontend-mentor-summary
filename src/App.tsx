11c37cc9272b0ce4c9d8c16138540624df1aa146
KceSecK <sd.dubo@gmail.com>
2021-11-12 21:29:39 -0300

    first upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import "bootstrap/dist/css/bootstrap.css";
+import Card from "./components/Card";
+import "./assets/css/custom.scss";
+import backDesktop from "./assets/icons/pattern-background-desktop.svg";
+import backMobile from "./assets/icons/pattern-background-mobile.svg";
 
 function App() {
+  const divStyle = {
+    backgroundImage: `url(${backDesktop})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid div-size" style={divStyle}>
+      <div className="row centered">
+        <div className="col">
+          <Card />
+        </div>
+      </div>
+      <div className="fixed-bottom text-center">
+        Order Summary You can now listen to millions of songs, audiobooks, and
+        podcasts on any device anywhere you like! Annual Plan $59.99/year Change
+        Proceed to Payment Cancel Order
+        <div className="attribution">
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io/challenges/order-summary-component-QlPmajDUj"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by <a href="https://www.frontendmentor.io/profile/KceSecK">Kceseck</a>.
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import cardImg from "../assets/icons/illustration-hero.svg";
+import musicIco from "../assets/icons/icon-music.svg";
+
+function Card() {
+  return (
+    <div className="card custom-card overflow my-4 shadow">
+      <img src={cardImg} className="img-fluid" alt="" />
+      <div className="card-body text-center">
+        <h3 className="card-title p-3 fw-bold">Order Summary</h3>
+        <p className="card-text text-secondary">
+          You can now listen to millions of songs, <br />
+          audiobooks, and podcasts on any device <br />
+          anywhere you like!
+        </p>
+        <div className="row p-3">
+          <div className="col-4">
+            <img src={musicIco} alt="" />
+          </div>
+          <div className="col-4">
+            <div className="row fw-bold">Annual Plan</div>
+            <div className="row text-secondary">$59.99/year</div>
+          </div>
+          <div className="col-4 fw-bold">
+            <a href="/">Change</a>
+          </div>
+        </div>
+        <div className="d-grid gap-2 col-11 mx-auto m-3">
+          <a href="/" className="btn btn-custom fw-bold">
+            Procede to Payment
+          </a>
+          <a href="/" className="btn btn-cancel fw-bold">
+            Cancel Order
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
